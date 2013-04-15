f644a662b2404c4281596dcb944c3cbc624e3eac
Karen Olvera <kareneolvera@hotmail.com>
2013-04-15 18:34:27 -0500

    Categorias y restaurantes dinamicos
    Se cargan las categorias y los restaurantes de manera dinámica

--- src/com/example/wheretoeat/SampleExpandableListAdapter.java
+++ src/com/example/wheretoeat/SampleExpandableListAdapter.java
@@ -27,14 +27,23 @@ public class SampleExpandableListAdapter extends BaseExpandableListAdapter imple
     private static final int GROUP_ITEM_RESOURCE = R.layout.groupitem;
     private static final int CHILD_ITEM_RESOURCE = R.layout.explistchild;
     List<Category> categories = null;
-    List<Restaurant> restaurants = null;
+    
+    List<Restaurant> comidaRapida = null;
+    List<Restaurant> comidaOriental = null;
+    List<Restaurant> ensaladas = null;
+    List<Restaurant> cafeterias = null;
+    List<Restaurant> pastelerias = null;
+    List<Restaurant> buffet = null;
+    List<Restaurant> mariscos = null;
+    List<Restaurant> barGrill = null;
+    List<Restaurant> comidaItaliana = null;
+    List<Restaurant> comidaMexicana = null;
+    List<Restaurant> pizzas = null;
+    List<Restaurant> otros = null;
+    
+    
     databaseHandler db;
-    private static final String[][] data = 
-	{
-		{"Pollo Loco","Starbucks","Carls Jr"},
-		{"Mc Donalds","Costeñito"},
-		{"KFC","Las Alitas","Super Salads"}
-	};
+    
   
     //constructor
     public SampleExpandableListAdapter(Context context, Activity activity) {
@@ -42,13 +51,32 @@ public class SampleExpandableListAdapter extends BaseExpandableListAdapter imple
         this.context = context;
         db = new databaseHandler(this.context);
         updateRestaurants();
-        restaurants = db.getAllRestaurants();
+        //restaurants = db.getAllRestaurants();
         vi = (LayoutInflater) activity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        _objInt = data.length;
+        _objInt = categories.size();
     }
     
     public void updateRestaurants(){
+    	int category = 0;
     	categories = db.getAllCategories();
+    	Log.i("CATEGORIES", "CATEGORIES = " + categories.size());
+    	for(int i = 0; i < categories.size(); i++){
+    		category = categories.get(i).id;
+    		switch(category){
+    			case 1: comidaRapida = db.getByCategory(1); break;
+    			case 2: comidaOriental = db.getByCategory(2); break;
+    			case 3: ensaladas = db.getByCategory(3); break;
+    			case 4: cafeterias = db.getByCategory(4); break;
+    			case 5: pastelerias = db.getByCategory(5); break;
+    			case 6: buffet = db.getByCategory(6); break;
+    			case 7: mariscos = db.getByCategory(7); break;
+    			case 8: barGrill = db.getByCategory(8); break;
+    			case 9: comidaItaliana = db.getByCategory(9); break;
+    			case 10: comidaMexicana = db.getByCategory(10); break;
+    			case 11: pizzas = db.getByCategory(11); break;
+    			case 12: otros = db.getByCategory(12); break;
+    		}
+    	}
     }
 
     /**getChild: obtiene el hijo del expandable list
@@ -57,7 +85,8 @@ public class SampleExpandableListAdapter extends BaseExpandableListAdapter imple
      * @return data
      */
     public String getChild(int groupPosition, int childPosition) {
-        return data[groupPosition][childPosition];
+        //return data[groupPosition][childPosition];
+        return "PRueba";
     }
 
     /**getChildId : obtiene el id un hijo del expandable list
@@ -75,7 +104,24 @@ public class SampleExpandableListAdapter extends BaseExpandableListAdapter imple
      * @return data 
      */
     public int getChildrenCount(int groupPosition) {
-        return data[groupPosition].length;
+        //return data[groupPosition].length;
+        int count = 0;
+        int category = categories.get(groupPosition).id;
+    	switch(category){
+			case 1: count = comidaRapida.size(); break;
+			case 2: count = comidaOriental.size(); break;
+			case 3: count = ensaladas.size(); break;
+			case 4: count = cafeterias.size(); break;
+			case 5: count = pastelerias.size(); break;
+			case 6: count = buffet.size(); break;
+			case 7: count = mariscos.size(); break;
+			case 8: count = barGrill.size(); break;
+			case 9: count = comidaItaliana.size(); break;
+			case 10: count = comidaMexicana.size(); break;
+			case 11: count = pizzas.size(); break;
+			case 12: count = otros.size(); break;
+		}
+        return count;
     }
 
 
@@ -89,56 +135,89 @@ public class SampleExpandableListAdapter extends BaseExpandableListAdapter imple
      */
     public View getChildView(int groupPosition, int childPosition, boolean isLastChild, View convertView, ViewGroup parent) {
         View v = convertView;
-        String child = getChild(groupPosition, childPosition);
-       // int id_res = 0;
+        //String child = getChild(groupPosition, childPosition);
+        //String child = null;
+        int category = categories.get(groupPosition).id;
+        //int id_res = 0;
+        String restaurantName = "";
         
         Log.i("EXPANDABLE", "CHILD = " + childPosition);
+        Log.i("EXPANDABLE", "CHILD CATEGORY = " + category);
+
+		switch(category){
+			case 1: 
+				restaurantName = comidaRapida.get(childPosition).restaurant + " " + comidaRapida.get(childPosition).sucursal;
+				break;
+			case 2: 
+				restaurantName = comidaOriental.get(childPosition).restaurant + " " + comidaOriental.get(childPosition).sucursal;
+				break;
+			case 3: 
+				restaurantName = ensaladas.get(childPosition).restaurant + " " + ensaladas.get(childPosition).sucursal;
+				break;
+			case 4: 
+				restaurantName = cafeterias.get(childPosition).restaurant + " " + cafeterias.get(childPosition).sucursal;
+				break;
+			case 5: 
+				restaurantName = pastelerias.get(childPosition).restaurant + " " + pastelerias.get(childPosition).sucursal;
+				break;
+			case 6: 				
+				restaurantName = buffet.get(childPosition).restaurant + " " + buffet.get(childPosition).sucursal;
+				break;
+			case 7: 
+				restaurantName = mariscos.get(childPosition).restaurant + " " + mariscos.get(childPosition).sucursal;
+				break;
+			case 8: 
+				restaurantName = barGrill.get(childPosition).restaurant + " " + barGrill.get(childPosition).sucursal;
+				break;
+			case 9: 
+				restaurantName = comidaItaliana.get(childPosition).restaurant + " " + comidaItaliana.get(childPosition).sucursal;
+				break;
+			case 10: 
+				restaurantName = comidaMexicana.get(childPosition).restaurant + " " + comidaMexicana.get(childPosition).sucursal;
+				break;
+			case 11: 
+				restaurantName = pizzas.get(childPosition).restaurant + " " + pizzas.get(childPosition).sucursal;
+				break;
+			case 12: 
+				restaurantName = otros.get(childPosition).restaurant + " " + otros.get(childPosition).sucursal;
+				break;
+		}
+		
+		v = vi.inflate(CHILD_ITEM_RESOURCE, null);
+        ViewHolder holder = new ViewHolder(v);
+        holder.text.setText(Html.fromHtml(restaurantName));
+        holder.imageview.setImageResource(R.drawable.salads);
+    	
+        /*if (child != null) {
+            v = vi.inflate(CHILD_ITEM_RESOURCE, null);
+            ViewHolder holder = new ViewHolder(v);
+            holder.text.setText(Html.fromHtml("Hola"));
+            holder.imageview.setImageResource(R.drawable.salads);
+        }*/
         
-        if(groupPosition == 0){
-        	restaurants = db.getByCategory(0);
-        	Log.i("DATABASE", "DATABASE restaurants length = " + restaurants.size());
-        	//if(childPosition == 0) id_res = R.drawable.polloloco;
-            //if(childPosition == 1) id_res = R.drawable.starbucks;
-            //if(childPosition == 2) id_res = R.drawable.carls;
-        	if (child != null && restaurants != null) {
+        /*if(groupPosition == 0){
+        	if (child != null) {
                 v = vi.inflate(CHILD_ITEM_RESOURCE, null);
                 ViewHolder holder = new ViewHolder(v);
-                holder.text.setText(Html.fromHtml(restaurants.get(childPosition).restaurant + " " + restaurants.get(childPosition).sucursal));
+                holder.text.setText(Html.fromHtml("Hola"));
                 holder.imageview.setImageResource(R.drawable.salads);
             }
-        }
-        else if(groupPosition == 1){
-        	restaurants = db.getByCategory(1);
-        	Log.i("DATABASE", "DATABASE restaurants length = " + restaurants.size());
-            //if(childPosition == 0) id_res = R.drawable.mcdonalds;
-            //if(childPosition == 1) id_res = R.drawable.costeno;
-        	if (child != null && restaurants != null) {
+        }else if(groupPosition == 1){
+        	if (child != null) {
                 v = vi.inflate(CHILD_ITEM_RESOURCE, null);
                 ViewHolder holder = new ViewHolder(v);
-                holder.text.setText(Html.fromHtml(restaurants.get(childPosition).restaurant + " " + restaurants.get(childPosition).sucursal));
+                holder.text.setText(Html.fromHtml("Hola"));
                 holder.imageview.setImageResource(R.drawable.salads);
             }
-        }
-        else if(groupPosition == 2){
-        	restaurants = db.getByCategory(2);  
-        	Log.i("DATABASE", "DATABASE restaurants length = " + restaurants.size());
-        	//if(childPosition == 0) id_res = R.drawable.kfc;
-            //if(childPosition == 1) id_res = R.drawable.alitas;
-            //if(childPosition == 2) id_res = R.drawable.salads;
-        	if (child != null && restaurants != null) {
+        }else if(groupPosition == 2){
+        	if (child != null) {
                 v = vi.inflate(CHILD_ITEM_RESOURCE, null);
                 ViewHolder holder = new ViewHolder(v);
-                holder.text.setText(Html.fromHtml(restaurants.get(childPosition).restaurant + " " + restaurants.get(childPosition).sucursal));
+                holder.text.setText(Html.fromHtml("Hola"));
                 holder.imageview.setImageResource(R.drawable.salads);
             }
-        }
-       
-        //if (child != null) {
-          //  v = vi.inflate(CHILD_ITEM_RESOURCE, null);
-          //  ViewHolder holder = new ViewHolder(v);
-           // holder.text.setText(Html.fromHtml(child));
-           // holder.imageview.setImageResource(id_res);
-        //}
+        }*/
+
         return v;
     }
     
@@ -157,7 +236,7 @@ public class SampleExpandableListAdapter extends BaseExpandableListAdapter imple
      * @return cantidad de grupos 
      */
     public int getGroupCount() {
-        return data.length;
+        return categories.size();
     }
     
     /**getGroupId : obtiene el id del grupo
@@ -179,12 +258,11 @@ public class SampleExpandableListAdapter extends BaseExpandableListAdapter imple
     public View getGroupView(int groupPosition, boolean isExpanded, View convertView, ViewGroup parent) {
         View v = convertView;
         String group = null;
-        int id_res = 0;
-        long group_id = getGroupId(groupPosition);
+        //int id_res = 0;
+        //long group_id = getGroupId(groupPosition);
        
         Log.i("EXPANDABLE", "GROUP = " + groupPosition);
-        
-        if(group_id == 0){
+       /* if(group_id == 0){
                 group = "Cafeterias";
                 id_res = R.drawable.cafe;
         }
@@ -195,14 +273,16 @@ public class SampleExpandableListAdapter extends BaseExpandableListAdapter imple
         else if(group_id == 2){
                 group = "FastFood";
                 id_res = R.drawable.comidarapida;
-        }
+        }*/
        
+        group = categories.get(groupPosition).category;
+        Log.i("CATEGORIES", "CATEGORY = " + group);
         if (group != null) {
             v = vi.inflate(GROUP_ITEM_RESOURCE, null);
             ViewHolder holder = new ViewHolder(v);
 
             holder.text.setText(Html.fromHtml(group));
-            holder.imageview.setImageResource(id_res);
+            holder.imageview.setImageResource(R.drawable.cafe);
              
         }
         return v;
